600dec815f54b181aa7016f0ff826002546f552a
Joe Greer <jgreer@proteinaceous.net>
2022-04-19 11:40:41 -0500

    add service and routing

---
+++ HubmapBlazor/Data/LiteratureProtein.cs
@@ -0,0 +1,40 @@
+﻿namespace HubmapBlazor.Data
+{
+    public class LiteratureProtein
+    {
+        public LiteratureProtein(string gene, string ensembl, string uniprotAccession, string geneDescription, string hpaEvidence,
+            string rnaTissueSpecificity, int rnaTissueSpecificityScore, string rnaTissueSpecificNX, string uniprotName, int mass,
+            int goAnnotations, int geneID, int pubmedCitations, int antibodyCount)
+        {
+            Gene = gene;
+            Ensembl = ensembl;
+            UniprotAccession = uniprotAccession;
+            GeneDescription = geneDescription;
+            HPAEvidence = hpaEvidence;
+            RNATissueSpecificity = rnaTissueSpecificity;
+            RNATissueSpecificityScore = rnaTissueSpecificityScore;
+            RNATissueSpecicNX = rnaTissueSpecificNX;
+            UniprotName = uniprotName;
+            Mass = mass;
+            GOAnnotations = goAnnotations;
+            GeneID = geneID;
+            PubmedCitations = pubmedCitations;
+            AntibodyCount = antibodyCount;
+        }
+
+        public string Gene { get; set; }
+        public string Ensembl { get; set; }
+        public string UniprotAccession { get; set; }
+        public string GeneDescription { get; set; }
+        public string HPAEvidence { get; set; }
+        public string RNATissueSpecificity { get; set; }
+        public int RNATissueSpecificityScore { get; set; }
+        public string RNATissueSpecicNX { get; set; }
+        public string UniprotName { get; set; }
+        public int Mass { get; set; }
+        public int GOAnnotations { get; set; }
+        public int GeneID { get; set; }
+        public int PubmedCitations { get; set; }
+        public int AntibodyCount { get; set; }
+    }
+}
---
+++ HubmapBlazor/Data/LiteratureProteinService.cs
@@ -0,0 +1,80 @@
+﻿namespace HubmapBlazor.Data
+{
+    public class LiteratureProteinService
+    {
+        private ICollection<LiteratureProtein> _literatures = new List<LiteratureProtein>();
+
+        public LiteratureProteinService(string file)
+        {
+            using (StreamReader reader = new StreamReader(file))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var lines = reader.ReadLine();
+                    var values = lines.Split(",");
+
+                    int toInt(string value)
+                    {
+                        if (int.TryParse(value, out int i))
+                        {
+                            return i;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+
+                    var s = new LiteratureProtein(
+                        values[0],
+                        values[1],
+                        values[2],
+                        values[3],
+                        values[4],
+                        values[5],
+                        toInt(values[6]),
+                        values[7],
+                        values[8],
+                        toInt(values[9]),
+                        toInt(values[10]),
+                        toInt(values[11]),
+                        toInt(values[12]),
+                        toInt(values[13]));
+
+                    _literatures.Add(s);
+                }
+            }
+        }
+
+
+        public Task<LiteratureProtein[]> GetLiteratureAsync(IEnumerable<string> accessions)
+        {
+            var literatures = new List<LiteratureProtein>();
+
+            return (Task<LiteratureProtein[]>)Task.Run(() => 
+            {
+                foreach (var accession in accessions)
+                {
+                    literatures.AddRange(_literatures.Where(x => x.UniprotAccession == accession));
+                }
+
+                literatures.ToArray();
+            });
+        }
+
+        public IEnumerable<LiteratureProtein> GetTissueAccessions(IEnumerable<string> accessions)
+        {
+            var list = new List<LiteratureProtein>();
+            foreach (var accession in accessions)
+            {
+                var proteins = _literatures.Where(x => x.UniprotAccession == accession);
+                foreach (var protein in proteins)
+                {
+                    list.Add(protein);
+                }
+            }
+
+            return list;
+        }
+    }
+}
