844a95d294a91577647d708049ac42b8c0707939
Joe Greer <jgreer@proteinaceous.net>
2022-05-05 09:55:44 -0500

    speed up a little

--- HubmapBlazor/Data/TissueProvider.cs
+++ HubmapBlazor/Data/TissueProvider.cs
@@ -6,15 +6,16 @@ namespace HubmapBlazor.Data
         private ICollection<Tissue> tissueInfos = new List<Tissue>();
         public TissueProvider(BupService bupService)
 		{
-            var Tissues = bupService.TissueList();
+            var tissues = bupService.TissueList();
+            var bupInfos = bupService.GetBupInfos();
 
-            foreach (var tissue in Tissues)
+            foreach (var tissue in tissues)
             {
                 var bupTissues = bupService.GetBupsWithTissue(tissue);
                 var abundantBups = bupService.GetAbundantBups(bupTissues);
                 var proteinCount = abundantBups.Count();
 
-                var uniqueCount = bupService.GetUniqueProteins(tissue, bupService.GetAbundantBups(bupService.GetBupInfos()));
+                var uniqueCount = bupService.GetUniqueProteins(tissue, bupService.GetAbundantBups(bupInfos));
 
                 Tissue tissue1 = new Tissue(tissue, proteinCount, uniqueCount);
 
