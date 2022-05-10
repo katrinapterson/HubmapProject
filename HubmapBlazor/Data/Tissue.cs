2a446680d1e88d579c0aed3cb734f41e099f0a15
Joe Greer <jgreer@proteinaceous.net>
2022-05-10 12:12:12 -0500

    add sorting on tissues

--- HubmapBlazor/Data/Tissue.cs
+++ HubmapBlazor/Data/Tissue.cs
@@ -8,9 +8,12 @@ namespace HubmapBlazor.Data
 			Name = name;
 			Proteins = proteins;
 			UniqueProteins = uniqueProteins;
+			UniqueProteinsCount = uniqueProteins.Count();
 		}
 		public string Name { get; set; }
 		public int Proteins { get; set; }
+
+		public int UniqueProteinsCount { get; set; }
 		public List<string> UniqueProteins { get; set; }
 	}
 }
