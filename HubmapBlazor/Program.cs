cea64a748e6c743863109d4921dffa50cb3d69d9
Joe Greer <jgreer@proteinaceous.net>
2022-05-31 09:29:16 -0500

    updates

--- HubmapBlazor/Program.cs
+++ HubmapBlazor/Program.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using HubmapBlazor.Data;
-using HubmapProject;
 using MudBlazor;
 using MudBlazor.Services;
 
 
-var bupService = new BupService(@"../HubmapProject/Resources/NewBupData");
-var literatureProteinService = new LiteratureProteinService(@"../HubmapProject/Resources/NewestLiteratureData");
+var bupService = new BupService(@"Resources/NewBupData");
+var literatureProteinService = new LiteratureProteinService(@"Resources/NewestLiteratureData");
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,13 +14,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-builder.Services.AddSingleton(_ => new BupInformationProvider("file path"));
-builder.Services.AddSingleton(_ => new AntibodyCountProvider("file path"));
-builder.Services.AddSingleton(_ => new AntibodyTableProvider("file path"));
-builder.Services.AddSingleton(_ => new LiteratureInfoProvider(@"../HubmapProject/Resources/NewestLiteratureData"));
+//builder.Services.AddSingleton(_ => new BupInformationProvider("file path"));
+//builder.Services.AddSingleton(_ => new AntibodyCountProvider("file path"));
+//builder.Services.AddSingleton(_ => new AntibodyTableProvider("file path"));
+//builder.Services.AddSingleton(_ => new LiteratureInfoProvider(@"Resources/NewestLiteratureData"));
 builder.Services.AddSingleton(_ => bupService);
 builder.Services.AddSingleton(_ => literatureProteinService);
-builder.Services.AddSingleton(_ => new AntibodyService(@"../HubmapProject/Resources/antibody_table_unconjugated.tsv"));
+builder.Services.AddSingleton(_ => new AntibodyService(@"Resources/antibody_table_unconjugated.tsv"));
 builder.Services.AddSingleton(_ => new TissueProvider(bupService, literatureProteinService));
 
 
